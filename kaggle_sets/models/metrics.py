57124882051360a390d819bab53ed8b9c7b70e5c
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-12-03 21:03:53 +0200

    using numpy as np

--- kaggle_sets/models/metrics.py
+++ kaggle_sets/models/metrics.py
@@ -1,25 +1,20 @@
 from enum import Enum
-from numpy import (
-    ndarray,
-    abs as np_abs,
-    sum as np_sum,
-    mean as np_mean
-)
+import numpy as np
 
 
 class Metric:
-    def _metric(self, prediction: ndarray, y: ndarray):
+    def _metric(self, prediction: np.ndarray, y: np.ndarray):
         """
         :return: value of a metric
         """
     
-    def __call__(self, prediction: ndarray, y: ndarray):
+    def __call__(self, prediction: np.ndarray, y: np.ndarray):
         return self._metric(prediction, y)
 
 
 class MSE(Metric):
     """ Mean Squared Error Metric"""
-    def _metric(self, prediction: ndarray, y: ndarray):
+    def _metric(self, prediction: np.ndarray, y: np.ndarray):
         diff = prediction - y
         m = diff.shape[1]
         return (1 / m) * (diff @ diff.T)[0, 0]
@@ -27,16 +22,16 @@ class MSE(Metric):
 
 class MAE(Metric):
     """ Mean Absolute Error Metric"""
-    def _metric(self, prediction: ndarray, y: ndarray):
+    def _metric(self, prediction: np.ndarray, y: np.ndarray):
         m = y.shape[0]
-        diff = np_abs(prediction - y)
-        return np_sum(diff, axis=0) * (1 / m)
+        diff = np.abs(prediction - y)
+        return np.abs(diff, axis=0) * (1 / m)
 
 
 class Accuracy(Metric):
     """ Accuracy Metric"""
-    def _metric(self, prediction: ndarray, y: ndarray):
-        correct_predictions = np_sum(prediction == y)
+    def _metric(self, prediction: np.ndarray, y: np.ndarray):
+        correct_predictions = np.abs(prediction == y)
         total_predictions = len(y)
         accuracy = correct_predictions / total_predictions if total_predictions > 0 else 0.0
         return accuracy
@@ -44,10 +39,10 @@ class Accuracy(Metric):
 
 class R2(Metric):
     """ R Squared Metric """
-    def _metric(self, prediction: ndarray, y: ndarray):
-        mean_y = np_mean(y)
-        ss_total = np_sum((y - mean_y) ** 2)
-        ss_residual = np_sum((y - prediction) ** 2)
+    def _metric(self, prediction: np.ndarray, y: np.ndarray):
+        mean_y = np.mean(y)
+        ss_total = np.abs((y - mean_y) ** 2)
+        ss_residual = np.abs((y - prediction) ** 2)
         r2 = 1 - (ss_residual / ss_total) if ss_total != 0 else 0.0
         return r2
 
