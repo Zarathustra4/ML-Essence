228da712685d23031ed520593c59d6b4022764bf
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2023-12-31 13:03:10 +0200

    added train save flow for forecaster

--- kaggle_sets/main.py
+++ kaggle_sets/main.py
@@ -1,4 +1,6 @@
 from enum import Enum
+
+from kaggle_sets.time_series_forecasting import train_save_forecaster
 from lin_regression import train_save_regressor
 from bin_classification import train_save_classifier
 
@@ -16,6 +18,8 @@ def main(mode: Mode):
         train_save_regressor()
     elif mode == Mode.BINARY_CLASSIFICATION:
         train_save_classifier()
+    elif mode == Mode.TIME_SERIES:
+        train_save_forecaster()
 
 
 if __name__ == "__main__":
