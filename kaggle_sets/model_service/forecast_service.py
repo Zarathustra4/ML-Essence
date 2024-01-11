8003c6a6c7dd99ff90fd62cf2be98f0d806ecd4c
Max Voloshchuk <maxvoloshchuk1@gmail.com>
2024-01-11 17:08:27 +0200

    epoch number is changed to 85

--- kaggle_sets/model_service/forecast_service.py
+++ kaggle_sets/model_service/forecast_service.py
@@ -65,7 +65,7 @@ class ForecastService:
 def train_save_forecaster():
     service = ForecastService()
     service.reset_model()
-    service.train_model(epochs=40)
+    service.train_model(epochs=85)
 
     print("| --- Testing model --- |")
     print(service.test_model())
